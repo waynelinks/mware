f14a37b12acedd1c276169c459ae37bce0f81f4d
Wayne Links <wayne.links@gmail.com>
2020-07-12 01:25:45 +0200

    fix: fix import statement

--- src/middleware/requireAuth.ts
+++ src/middleware/requireAuth.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 
-import { UNAUTHORIZED_ERROR } from '../errors/UnAuthorizedError'
+import { UnAuthorizedError } from '../errors/UnAuthorizedError'
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
-  if (!req.currentUser) throw new UNAUTHORIZED_ERROR()
+  if (!req.currentUser) throw new UnAuthorizedError()
 
   next()
 }
